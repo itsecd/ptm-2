db17501d22d24dc8f8ecc5af2af019bee3a05a8f
alxmcs <70561974+alxmcs@users.noreply.github.com>
2023-11-11 22:45:33 +0400

    initial

---
+++ cash_machine.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from branch_finder import BranchFinder
+
+
+class CashMachine:
+    @property
+    def branch_finder(self):
+        return BranchFinder()
+
+    def withdraw(self, town: str, customer: str, amount: Decimal):
+        return self.branch_finder 
+            .find_branch_for_town(town) 
+            .personal_accounts() 
+            .get_account_for_customer(customer) 
+            .withdraw(amount)
